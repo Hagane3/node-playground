14a4d9f93bbba38208ca6d87742275f0c468f904
Konrad <konslo96@gmail.com>
2023-05-06 11:04:23 +0200

    404 handle

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ app.get("/contact", (req, res) => {
   res.sendFile("./views/contact.html", { root: __dirname });
 });
 
+// redirects
+
 app.get("/contact-us", (req, res) => {
   res.redirect("/contact");
+});
+
+// 404 page
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
 
 app.listen(3000);
