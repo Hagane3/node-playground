8debb4be87c64b153dc5b670520d3fa5a2f7c16c
Konrad <konslo96@gmail.com>
2023-05-06 13:57:26 +0200

    establish mongodb connection

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
+// connect to mongodb
+
+const dbURI =
+  "mongodb+srv://user:user123@nodetuts.qwl7sbq.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // listen for requests
-app.listen(3000);
 
 // register view engine
 app.set("view engine", "ejs");
