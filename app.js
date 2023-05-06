ef5098b6fa043c98a1c7a0e3515eb6cbb8b79ee1
Konrad <konslo96@gmail.com>
2023-05-06 15:27:38 +0200

    add routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+
+const blogRoutes = require("./routes/blogRoutes");
 
 // express app
 const app = express();
@@ -38,27 +39,9 @@ app.use((req, res, next) => {
 
 app.use(morgan("dev"));
 
+app.use(blogRoutes);
+
 // mongoose and mongo sandbox routes
-
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/single-blog", (req, res) => {
-  Blog.findById("645644f0bceda9fabf4b703b")
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
@@ -71,50 +54,6 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
-});
-
-// blog routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.render("index", { title: "All blogs", blogs: result });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => console.log(err));
 });
 
 // 404 page
