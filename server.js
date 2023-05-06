94c089d62bc91f4e3246d61704d7d492f18e3c9c
Konrad <konslo96@gmail.com>
2023-05-06 09:47:39 +0200

    add basic lodash expressions

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // lodash
 
-  // set header content type
+  const num = _.random(0, 20);
+  console.log(num);
+
+  const greet = _.once(() => {
+    console.log("hello");
+  });
+
+  greet();
 
   res.setHeader("Content-Type", "text/html");
 
